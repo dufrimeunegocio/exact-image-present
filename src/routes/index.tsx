124516dcb0413fde0aaa950659e8a84e8cac38ee
dufrimeunegocio <dufrimeunegocio@gmail.com>
2026-09-24 17:18:57 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,6 +11,8 @@ import {
 import { Reveal } from "@/components/Reveal";
 import logoAsset from "@/assets/karen-keller-logo.png.asset.json";
 import karenAsset from "@/assets/karen.jpg.asset.json";
+import karenAsset from "@/assets/karen-keller-atendendo.jpeg";
+
 
 const WHATSAPP = "https://wa.me/5548974008889";
 const INSTAGRAM = "https://instagram.com/karen_keller_neuropsico";
